70ba4e8a5b2f750b4e735d51a549d20a714fce77
kecwink <46334745+kecwink@users.noreply.github.com>
2020-04-03 12:42:08 -0600

    Add files via upload

--- plot.js
+++ plot.js
@@ -1,36 +1,159 @@
-metadata=[{"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0}, {"id": 941, "ethnicity": "Caucasian/Midleastern", "gender": "F", "age": 34.0, "location": "Chicago/IL", "bbtype": "I", "wfreq": 1.0}, {"id": 943, "ethnicity": "Caucasian", "gender": "F", "age": 49.0, "location": "Omaha/NE", "bbtype": "I", "wfreq": 1.0}, {"id": 944, "ethnicity": "European", "gender": "M", "age": 44.0, "location": "NewHaven/CT", "bbtype": "I", "wfreq": 1.0}, {"id": 945, "ethnicity": "Caucasian", "gender": "F", "age": 48.0, "location": "Philidelphia/PA", "bbtype": "I", "wfreq": 1.0}, {"id": 946, "ethnicity": "Caucasian", "gender": "F", "age": 42.0, "location": "Deerfield/MA", "bbtype": "I", "wfreq": 3.0}]
-names=["940", "941", "943", "944", "945", "946", "947", "948", "949", "950", "952", "953", "954", "955", "956", "958", "959", "960", "961", "962", "963", "964", "966", "967", "968", "969", "970", "971", "972", "973", "974", "975", "978", "1233", "1234", "1235", "1236", "1237", "1238", "1242", "1243", "1246", "1253", "1254", "1258", "1259", "1260", "1264", "1265", "1273", "1275", "1276", "1277", "1278", "1279", "1280", "1281", "1282", "1283", "1284", "1285", "1286", "1287", "1288", "1289", "1290", "1291", "1292", "1293", "1294", "1295", "1296", "1297", "1298", "1308", "1309", "1310", "1374", "1415", "1439", "1441", "1443", "1486", "1487", "1489", "1490", "1491", "1494", "1495", "1497", "1499", "1500", "1501", "1502", "1503", "1504", "1505", "1506", "1507", "1508", "1510", "1511", "1512", "1513", "1514", "1515", "1516", "1517", "1518", "1519", "1521", "1524", "1526", "1527", "1530", "1531", "1532", "1533", "1534", "1535", "1536", "1537", "1539", "1540", "1541", "1542", "1543", "1544", "1545", "1546", "1547", "1548", "1549", "1550", "1551", "1552", "1553", "1554", "1555", "1556", "1557", "1558", "1561", "1562", "1563", "1564", "1572", "1573", "1574", "1576", "1577", "1581", "1601"]
+var allData;
 
-function init(){
+//read the data
+d3.json('samples.json').then((data) => {
+    //console.log(data) 
+
+    allData = data;
+    init()
+});
+
+function init() {
     //select dropDown menu
     var dropDown = d3.select('#selTestSubject');
-   
-    //read the data
-    d3.json('data/samples.json').then((data)=>{
-        //console.log(data) 
-        //get the id data to the dropdown menu
-        data.names.forEach(function(name){
-            dropDown.append('option').text(name).property('value');
-        });
-       
-        });
-        
+    //get the id data to the dropdown menu
+    allData.names.forEach(function (name) {
+        dropDown.append('option').text(name).property('value', name);
+    });
+    //print the first sample metadata to the home screen
+    getInfo(allData.names[0])
+};
+function getInfo(sample) {
+
+    //select demographic box
+    var demoBox = d3.select('#demographicInfo');
+
+    //clear the previous information
+    demoBox.html("")
+
+    //loop through all the metadata and find the object with the id that matches sample
+    //[0] gives the found object as an object, not an array of arrays
+    var data = allData.metadata.filter(obj => obj.id == sample)[0]
+
+    //print each piece of the metadata under the dropdown box
+    Object.entries(data).forEach(([key, value]) => demoBox.append('p').text(`${key} : ${value}`));
+    //  console.log(`${key} : ${value}`));
+
+
+
+    getPlot(allData.names[0])
+};
+
+function getPlot(sample) {
+
+    //select plot area
+    var barChart = d3.select('#sampleBacteria');
+
+    //clear the previous information
+    barChart.html("")
+
+    //find the sampledata that matches the sample
+    var data = allData.samples.filter(obj => obj.id == sample)[0]
+    //console.log(data)
+
+
+    //isolate the top ten most common bacteria
+    var bacteriaSamples = data.sample_values.slice(0, 10)
+
+    //isolate the top ten bacteria ids    
+    var graphLabel = []
+
+    //create an otu label and make each element of the bacteriaId array a string
+    var bacteriaId = data.otu_ids.slice(0, 10)
+    for (var j = 0; j < bacteriaId.length; j++) {
+        graphLabel.push(`OTU ${bacteriaId[j]}`)
     };
-    function getInfo(){
-        //select demographic box
-        var demoBox = d3.select('#demographicInfo');
-    
-        //read the data
-        d3.json('samples.json').then((data)=>{
-            //console.log(data.metadata )
-            for (var i=0; i < data.names.length; i ++){
-                Object.entries(data.metadata[i]).forEach(([key, value]) => demoBox.append('p').text(`${key} : ${value}`));
-                //  console.log(`${key} : ${value}`));
-            };
-            
-            });
-            
-        };
-  
- getInfo()
- 
+
+    //  Create the Traces
+    var trace1 = {
+        x: bacteriaSamples,
+        y: graphLabel,
+        type: "bar",
+        orientation: 'h',
+        text: bacteriaId
+    };
+
+
+    // Create the data array for the plot
+    var graphData = [trace1];
+
+
+    // Define the plot layout
+    var layout = {
+        xaxis: { title: "Number of Specimen" },
+    };
+
+    Plotly.newPlot("plot", graphData, layout);
+
+    getBubbleChart(allData.names[0])
+
+}
+
+//create a bubble chart
+function getBubbleChart(sample) {
+    //select plot area
+    var chartArea = d3.select('#bubbleChart');
+
+    //clear the previous information
+    chartArea.html("")
+
+    //find the sampledata that matches the sample
+    var data = allData.samples.filter(obj => obj.id == sample)[0]
+    //console.log(data)
+
+    //select the otu_id data
+    var bacteria = data.otu_ids
+    //console.log(bacteria.length)
+
+    //verify that all otu_ids are different
+    // var sameOtu = []
+    // for (var m = 0; m <bacteria.length; m++){
+    //     if (bacteria[m] != bacteria[m+1]){
+    //         continue
+    //     }
+    //     else{
+    //         sameOtu.push(bacteria[m])
+    //     }
+    //     }
+    // console.log(sameOtu)
+
+
+    //select the sample values
+    var samples = data.sample_values
+    //console.log(samples.length)
+
+    //create labels that have OTU added to the otu_id
+    var bacteriaIdLabel = []
+    for (var k = 0; k < bacteria.length; k++) {
+        bacteriaIdLabel.push(`OTU ${bacteria[k]}`)
+    };
+
+    // var bubbleColor = ['#d6cbd3','#eca1a6', '#bdcebe', '#ada397', '#d5e1df','#e3eaa7','#b5e7a0','#86af49','#b9936c','#dac292','#e6e2d3','#3e4444','#92a8d1','#034f84', '#f7786b', '#deeaee','#c94c4c','#ffef96','#50394c','#80ced6','#b2c2bf','#3b3a30','#b0aac0','#c2d4dd','#563f46','#625750', '#484f4f','#7e4a35','#cab577','#587e76','#a96e5b','#454140','#bd5734','','','', '', '','','','','','','','','','', '', '','','','','','','','','','','', '','','','','','','','','','','', '','','','','','','','','',''] 
+    //console.log(bubbleColor.length)
+
+    var trace1 = {
+        x: bacteria,
+        y: samples,
+        text: bacteriaIdLabel,
+        mode: 'markers',
+        marker: {
+            size: samples,
+            color: bacteria,
+        }
+    };
+    var data = [trace1];
+    var layout = {
+        title: 'OTU ID (Microbial Species)',
+        showlegend: false        
+    };
+    Plotly.newPlot('bubbleChart', data, layout)
+}
+
+//sampleID is a variable only defined within the onChange function and is set on the index.html page. it equals this.value, which is the value
+// selected in the drop down box
+
+function onChange(sampleID) {
+    getInfo(sampleID)
+    getPlot(sampleID)
+    getBubbleChart(sampleID)
+}
